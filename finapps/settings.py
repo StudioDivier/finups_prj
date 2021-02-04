8afac10aa260da5ba13f9824e360f39d1f0f25f1
StudioDivier <ianfoxstudio@gmail.com>
2021-02-04 18:47:52 +0300

    header/footer front done

--- finapps/settings.py
+++ finapps/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'site_app'
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
